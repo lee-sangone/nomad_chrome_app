8e85e7207f19600b7dbecc2276800ca4ef577320
lee-sangone <sangwon2613@naver.com>
2023-01-27 14:32:14 +0900

    clock update

--- js/script.js
+++ js/script.js
@@ -2,17 +2,28 @@ const loginInput = document.querySelector(".idInput")
 const loginForm = document.querySelector("#login-form")
 const greeting = document.querySelector("#greeting")
 const HIDDEN_CLASSNAME = "hidden"
+const USERNAME_KEY = "username"
 
 function loginBtnClick(event) {
     event.preventDefault()
     loginForm.classList.add("hidden")
     const username = loginInput.value
+    localStorage.setItem("USERNAME_KEY", username)
+    paintGreetings(username)
+}
+
+function paintGreetings(username) {
     greeting.innerText = `Hello ${username}`
     greeting.classList.remove(HIDDEN_CLASSNAME)
 }
 
-
-
 loginForm.addEventListener("submit", loginBtnClick)
 
+const savedUsername = localStorage.getItem("USERNAME_KEY")
 
+if (savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME)
+    loginForm.addEventListener("submit", loginBtnClick)
+} else {
+    paintGreetings(savedUsername)
+}
