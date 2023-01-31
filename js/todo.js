3308dc95a1ae4db09f7379104aa277e837b5fe65
lee-sangone <sangwon2613@naver.com>
2023-01-31 16:01:47 +0900

    todos update

--- js/todo.js
+++ js/todo.js
@@ -2,10 +2,36 @@ const toDoForm = document.querySelector("#todo-form")
 const toDoInput = toDoForm.querySelector("input")
 const toDoList = document.querySelector("#todo-list")
 
+const toDos = []
+
+function deleteToDo (event) {
+    const li = event.target.parentElement
+    li.remove()
+}
+
+function saveToDos () {
+    localStorage.setItem("todos", JSON.stringify(toDos))
+}
+
+function paintToDo(newToDo) {
+    const li = document.createElement("li")
+    const span = document.createElement("span")
+    const button = document.createElement("button")
+    button.innerText = "X"
+    button.addEventListener("click", deleteToDo)
+    li.appendChild(span)
+    li.appendChild(button)
+    span.innerText = newToDo
+    toDoList.appendChild(li)
+}
+
 function handleToDoSubmit (event) {
     event.preventDefault()
     const newToDo = toDoInput.value
     toDoInput.value = ""
+    toDos.push(newToDo)
+    paintToDo(newToDo)
+    saveToDos()
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit)
